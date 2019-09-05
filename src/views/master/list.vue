9b3927dfbbecf8641a5b4205a1a0ad4acfee228e
pllemon <nageshui071011>
2019-09-06 01:14:37 +0800

    更新

--- src/views/master/list.vue
+++ src/views/master/list.vue
@@ -1,109 +1,104 @@
 <template>
   <div class="app-container list-layout">
-    <!-- 搜索 -->
-    <el-form :inline="true" :model="queryMes" size="small">
-      <el-form-item label="申请月份">
-        <el-date-picker
-          v-model="queryMes.month"
-          type="month"
-          placeholder="选择月">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="审核状态">
-        <el-select v-model="queryMes.status" placeholder="请选择">
-          <el-option
-            v-for="(item, index) in statusOptions"
-            :key="index"
-            :label="item"
-            :value="index"
-          />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="师傅名">
-        <el-input v-model="queryMes.user" placeholder="请输入" />
-      </el-form-item>
-      <el-form-item label="师傅编号">
-        <el-input v-model="queryMes.user" placeholder="请输入" />
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="fetchData()">搜索</el-button>
-      </el-form-item>
-    </el-form>
-
-    <!-- 表格&分页 -->
+    <!-- 表头 -->
     <div class="table-header">
       <p class="title">师傅列表</p>
       <div class="action">
-        <el-button>批量导出</el-button>
+        <el-button size="small" icon="el-icon-upload2" round>批量导出</el-button>
       </div>
     </div>
+
     <div class="table-content">
-      <el-table
-        v-loading="listLoading"
-        :data="list"
-        element-loading-text="Loading"
-        border
-        fit
-        highlight-current-row
-        height="100%"
-        @selection-change="selectionChange"
-      >
-        <el-table-column type="selection" width="55" fixed />
-        <el-table-column type="index" width="50" />
-        <el-table-column align="center" label="姓名" />
-        <el-table-column align="center" label="身份证" />
-        <el-table-column align="center" label="手机号" />
-        <el-table-column align="center" label="入行年份" />
-        <el-table-column align="center" label="服务项目" />
-        <el-table-column align="center" label="联系地址" />
-        <el-table-column align="center" label="个人简介" />
-        <el-table-column align="center" label="申请时间" width="200">
-          <template slot-scope="scope">
-            <i class="el-icon-time" />
-            <span>{{ scope.row.display_time }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" label="操作" width="200" fixed="right">
-          <template slot-scope="scope">
-            <el-button type="text" @click="details(scope.$index, 1)">详情</el-button>
-            <el-button type="text" @click="details(scope.$index, 3)">编辑资料</el-button>
-            <el-button type="text" @click="details(scope.$index, 2)">通过</el-button>
-            <el-button type="text" @click="reject(scope.$index)">驳回</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <!-- 搜索 -->
+      <el-form :inline="true" :model="queryMes" size="small" class="search-form" ref="searchForm">
+        <el-form-item label="申请月份" prop="month">
+          <el-date-picker
+            v-model="queryMes.month"
+            type="month"
+            placeholder="选择月">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="审核状态">
+          <el-select v-model="queryMes.status" placeholder="请选择">
+            <el-option
+              v-for="(item, index) in statusOptions"
+              :key="index"
+              :label="item"
+              :value="index"
+            />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="师傅名">
+          <el-input v-model="queryMes.user" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item label="师傅编号">
+          <el-input v-model="queryMes.user" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="fetchData()">搜索</el-button>
+          <el-button @click="resetForm()">重置</el-button>
+        </el-form-item>
+      </el-form>
+
+      <!-- 表格&分页 -->
+      <div class="table-section">
+        <el-table
+          v-loading="listLoading"
+          :data="list"
+          element-loading-text="Loading"
+          border
+          fit
+          highlight-current-row
+          height="100%"
+          @selection-change="selectionChange"
+        >
+          <el-table-column type="selection" width="55" fixed />
+          <el-table-column type="index" width="50" />
+          <el-table-column align="center" label="姓名" />
+          <el-table-column align="center" label="身份证" />
+          <el-table-column align="center" label="手机号" />
+          <el-table-column align="center" label="入行年份" />
+          <el-table-column align="center" label="服务项目" />
+          <el-table-column align="center" label="联系地址" />
+          <el-table-column align="center" label="个人简介" />
+          <el-table-column align="center" label="申请时间" width="200">
+            <template slot-scope="scope">
+              <i class="el-icon-time" />
+              <span>{{ scope.row.display_time }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="操作" width="200" fixed="right">
+            <template slot-scope="scope">
+              <el-button type="text" @click="details(scope.$index, 1)">详情</el-button>
+              <el-button type="text" @click="details(scope.$index, 3)">编辑资料</el-button>
+              <el-button type="text" @click="details(scope.$index, 2)">通过</el-button>
+              <el-button type="text" @click="reject(scope.$index)">驳回</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <gd-pagination :total="total" :current-page="queryMes.page" :page-size="queryMes.limit" />
     </div>
-    <gd-pagination :total="total" :current-page="queryMes.page" :page-size="queryMes.limit" />
 
     <!-- 弹窗 -->
-    <el-dialog :title="dialogTitle" :visible="showDialog" width="1100px" :before-close="handleClose">
+    <!-- <el-dialog :title="dialogTitle" :visible="showDialog" width="1100px" :before-close="handleClose">
       <component :is="currentComponent" :dialogMes="dialogMes"/>
-    </el-dialog>
+    </el-dialog> -->
 
-    <!-- 驳回弹窗 -->
-    <el-dialog title="驳回申请" :visible="true" v-if="showReject" width="500px">
-      <p>确定驳回该申请？可填写驳回原因：</p>
-      <el-input
-        type="textarea"
-        :autosize="{ minRows: 2}"
-        placeholder="请输入"
-        v-model="rejectReason"
-      />
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="showReject = false">取 消</el-button>
-        <el-button type="primary" @click="submitReject">确 定</el-button>
-      </span>
-    </el-dialog>
+    <!-- 弹窗 -->
+    <component :is="currentComponent" :dialogMes="dialogMes"/>
   </div>
 </template>
 
 <script>
 import { getList } from '@/api/table'
 import Details from '@/views/master/details'
+import Reject from '@/views/master/reject'
 
 export default {
   components: {
-    Details
+    Details,
+    Reject
   },
   data() {
     return {
@@ -124,16 +119,17 @@ export default {
       currentComponent: '',
       showDialog: false,
       dialogTitle: '',
-      dialogMes: {},
-
-      showReject: false,
-      rejectReason: ''
+      dialogMes: {}
     }
   },
   created() {
     this.fetchData()
   },
   methods: {
+    resetForm(formName) {
+      this.$refs.searchForm.resetFields();
+    },
+
     fetchData() {
       this.listLoading = true
       getList().then(response => {
@@ -169,15 +165,12 @@ export default {
     },
 
     reject(id) {
-      this.activeId = id
-      this.showReject = true
+      this.currentComponent = 'Reject',
+      this.dialogMes = {
+        id: id
+      }
     },
-    submitReject() {
-      console.log(this.activeId)
-      console.log(this.rejectReason)
-      this.rejectReason = ''
-      this.fetchData()
-    }
+    
   }
 }
 </script>
---
+++ src/views/master/reject.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-dialog title="驳回申请" :visible="true" width="500px" :before-close="handleClose">
+    <p style="margin-bottom: 10px">确定驳回该申请？可填写驳回原因：</p>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 6}"
+      placeholder="请输入"
+      v-model="rejectReason"
+    />
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="handleClose">取 消</el-button>
+      <el-button type="primary" @click="submitReject">确 定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    dialogMes: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      rejectReason: ''
+    }
+  },
+  methods: {
+    handleClose() {
+      this.$parent.currentComponent = ''
+    },
+
+    submitReject() {
+      console.log(this.dialogMes.id)
+      console.log(this.rejectReason)
+      this.handleClose()
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
