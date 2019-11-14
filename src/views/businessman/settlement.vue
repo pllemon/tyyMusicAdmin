137802c8950187e5f071f5f5167eaf56bbfc45e5
pllemon <37794820+pllemon@users.noreply.github.com>
2019-11-14 10:06:21 +0800

    更新

--- src/views/businessman/settlement.vue
+++ src/views/businessman/settlement.vue
@@ -40,40 +40,25 @@
           height="100%"
           @selection-change="selectionChange"
         >
-          <el-table-column type="selection" width="55" fixed/>
-          <el-table-column label="序号" type="index" width="50" fixed/>
-          <el-table-column label="店铺名" prop="name" width="120"/>
-          <el-table-column label="联系方式" prop="phone" width="120"/>
-          <el-table-column label="店铺地址" prop="address" width="200"/>
-          <el-table-column label="门面图片" width="120">
-            <template slot-scope="scope">
-              <gd-image :src="scope.row.shopimg" small/>
-            </template>
-          </el-table-column>
-          <el-table-column label="产品图片" width="120">
-            <template slot-scope="scope">
-              <gd-image :src="scope.row.goodsimg" small/>
-            </template>
-          </el-table-column>
-          <el-table-column label="营业执照" width="120">
-            <template slot-scope="scope">
-              <gd-image :src="scope.row.businessimg" small/>
-            </template>
-          </el-table-column>
-          <el-table-column label="分享图片" width="120">
-            <template slot-scope="scope">
-              <gd-image :src="scope.row.sharewximg" small/>
-            </template>
-          </el-table-column>
+          <el-table-column type="selection" width="55" fixed />
+          <el-table-column label="序号" type="index" width="50" fixed />
+          <el-table-column label="店铺名" prop="name" width="120" />
+          <el-table-column label="联系方式" prop="phone" width="120" />
+          <el-table-column label="店铺地址" prop="address" />
+          <el-table-column label="总积分" width="200" prop="creattime" />
+          <el-table-column label="提现积分" width="200" prop="creattime" />
           <el-table-column label="申请时间" width="200" prop="creattime" />
-          <el-table-column label="状态" width="120">
+          <!-- <el-table-column label="备注" width="200" prop="creattime" /> -->
+          <el-table-column label="处理状态" width="200">
             <template slot-scope="scope">
               {{ recordStatus[scope.row.status] }}
             </template>
           </el-table-column>
-          <el-table-column label="审核时间" width="200" prop="examine" />
+          <el-table-column label="处理时间" width="200" prop="examine" />
           <el-table-column label="操作" width="200" fixed="right">
             <template slot-scope="scope">
+              <!-- <el-button type="text" @click="common.loadComponent(vm, 0, scope.row.id)">备注</el-button> -->
+              <el-button type="text" @click="common.loadComponent(vm, 0, scope.row.id)">已处理</el-button>
             </template>
           </el-table-column>
         </el-table>
